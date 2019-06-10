17be32e5af30878c43e986c1b5cfa77403027a97
James Lyons <jameslyons@Jamess-MacBook-Pro.local>
2019-06-10 16:38:01 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -33,16 +33,26 @@ function checkForMatch() {
 	}
 };
 
-function flipCard(cardId) {
-if (cardsInPlay.length === 2 && checkForMatch()) {
-		alert("You found a match!");
-	} else {
-	alert("sorry, try again.");
+function flipCard() {
+	var cardId = this.getAttribute('data-id', cardId);
+	cardsInPlay.push(cards[cardId]).rank;
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
 	}
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 };
-flipCard(0);
-flipCard(1);
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i)
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement)
+	}
+};
+createBoard();
